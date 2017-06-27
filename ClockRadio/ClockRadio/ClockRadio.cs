a34b099354598fafde398a443dcc585612602abc
wsiewert <wsiewert94@gmail.com>
2017-06-27 07:41:11 -0500

    First Commit

---
+++ ClockRadio/ClockRadio/ClockRadio.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockRadio
+{
+    class ClockRadio
+    {
+        //Member Varibales
+        public int radioStation;
+        public bool alarmIsSet;
+        public int alarmTime;
+        public DateTime time;
+
+
+        //Constructor
+        public ClockRadio()
+        {
+            time = DateTime.Now;
+            radioStation = 0;
+            alarmIsSet = false;
+            alarmTime = 12;
+        }
+
+        //Class Methods
+        public void getTime()
+        {
+            Console.WriteLine("Current Time: " + time);
+        }
+
+        public void setTime()
+        {
+            Console.WriteLine("Change to the hour to: ");
+            //string userInput = Console.ReadLine();
+            //double newHour = time.Hour;
+        }
+
+        public void getHelp()
+        {
+            Console.WriteLine("Clock Radio Commands:");
+            Console.WriteLine("'time' - Gets the current time.");
+            Console.WriteLine("'set' - Sets the hour. accepts 1-12 as input.");
+            Console.WriteLine("'station' - Gets the current radio station.");
+            Console.WriteLine("'change station' - Gets the current radio station.");
+        }
+
+        public void getStation()
+        {
+            Console.WriteLine(radioStation);
+        }
+
+        public void setStation()
+        {
+            string userInput = Console.ReadLine();
+            radioStation = int.Parse(userInput);
+        }
+    }
+}
---
+++ ClockRadio/ClockRadio/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockRadio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+
+            ClockRadio defaultClock = new ClockRadio();
+            Console.WriteLine("'Help' for a list of commands");
+            Console.WriteLine(defaultClock.time);
+
+            do
+            {
+                Console.WriteLine("");
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("Enter 'Exit' to quit the program.");
+                userInput = Console.ReadLine().ToLower();
+
+                switch (userInput)
+                {
+                    case "time":
+                        defaultClock.getTime();
+                        break;
+                    case "set":
+                        defaultClock.getTime();
+                        break;
+                    case "help":
+                        defaultClock.getHelp();
+                        break;
+                    case "station":
+                        defaultClock.getStation();
+                        break;
+                    case "change station":
+                        defaultClock.setStation();
+                        break;
+                    default:
+                        break;
+                }
+            } while (userInput != "exit");
+        }
+    }
+}
